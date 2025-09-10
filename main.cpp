9a58678c653bb9a63db347d6c63a087a4d80b9ed
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-10 09:12:32 +0300

    if

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-int main()
-{
-    int a = 20;
-    std::cout<<std::dec<<a<<std::endl;
-    std::cout<<std::hex<<a<<std::endl;
-    std::cout<<std::oct<<a<<std::endl;
-    std::cin.get();
+#include <math.h>
+using namespace std;
+
+int main() {
+    double a, x;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter x number: ";
+    cin >> x;
+
+    double y;
+    if (a >0 && x>0) {
+            y = sin(fabs(pow(a,x)));
+            cout << "version1 " << endl;
+    }
+    else if (a>0 && x<0) {
+        y = cos(fabs(exp(x)));
+        cout << "version 2 " << endl;
+    }
+    else {
+        y = sin(x)*cos(a);
+        cout << "version 3 " << endl;
+    }
+    cout << "y= " << y << endl;
+
     return 0;
 }
