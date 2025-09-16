59777ac455b4c64c4fe49783e6fdd03df807a66b
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-16 12:13:53 +0300

    for x = -2.0; x < 2 + E ver2

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@ using namespace std;
 int main() {
     const double E = 0.0001;
     const double hx = 0.5;
-    double a = 4;
     const double ha = 1.5;
-    for (double x = -2.0; x < 2 + E; x += hx) {
+    for (double x = -2.0, a = 4; x < 2 + E; x += hx, a += ha) {
         double y = exp(a) / pow(a, x);
         cout << "a =" << a << "\tx =" << x << "\ty = " << y << "\n";
-        a += ha;
     }
     cout << endl;
 
