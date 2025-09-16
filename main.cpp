f3cdab579e43e37fb58af9a5748d6c6e9d8ac9de
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-16 11:21:46 +0300

    find min

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
 using namespace std;
 
 int main() {
-    double a, x;
-    cout << "Enter a number: ";
-    cin >> a;
-    cout << "Enter x number: ";
-    cin >> x;
-
-    double y;
-    if (a >0 && x>0) {
-            y = sin(fabs(pow(a,x)));
-            cout << "version1 " << endl;
+    int a = 15;
+    int b = 7;
+    int min;
+    if (a < b) {
+        min = a;
+    } else {
+        min = b;
     }
-    else if (a>0 && x<0) {
-        y = cos(fabs(exp(x)));
-        cout << "version 2 " << endl;
-    }
-    else {
-        y = sin(x)*cos(a);
-        cout << "version 3 " << endl;
-    }
-    cout << "y= " << y << endl;
-
+    cout << min << endl;
     return 0;
 }
