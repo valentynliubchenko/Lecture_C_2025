ca09b556d0ac2e7c4b9301fe5e440500a898848b
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-16 11:54:41 +0300

    first for

--- main.cpp
+++ main.cpp
@@ -4,20 +4,15 @@
 using namespace std;
 int main()
 {
-    double a, x;
-    cout<<"Please input a =";
-    cin >> a;
-    cout<<"Please input x =";
-    cin >> x;
-    double y = (x > 0 && a>0) ? exp(pow(x,a)) : pow(x,exp(a)) ;
-    // if (x > 0 && a>0)
-    // {
-    //     y = exp(pow(x,a));
-    // }
-    // else
-    // {
-    //     y = pow(x,exp(a));
-    // }
-    cout<< "Result: y ="<<y<<endl;
+    int x = 0, y = 0;
+    for ( x=0; x<4; y = x, ++x) {
+        cout<<" current x = "<<x<<endl;
+        cout<<" current y = "<<y<<endl;
+    }
+    cout<<"x = "<<x<<endl;
+    cout<<"y = "<<y<<endl;
+
+
+    cout<<"Exit";
     return 0;
 }
