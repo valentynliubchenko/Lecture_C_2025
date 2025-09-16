b86e9b72137e3b5eda3013669f38dacf402acb24
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-16 11:25:21 +0300

    (a<b) ? a : b; ver2

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include <math.h>
 #include <stdlib.h>
 using namespace std;
-
-int main() {
-    int a = 5;
-    int b = 7;
-    int min = (a<b) ? a : b;
-
-    // if (a < b) {
-    //     min = a;
-    // } else {
-    //     min = b;
+int main()
+{
+    double a, x;
+    cout<<"Please input a =";
+    cin >> a;
+    cout<<"Please input x =";
+    cin >> x;
+    double y = (x > 0 && a>0) ? exp(pow(x,a)) : pow(x,exp(a)) ;
+    // if (x > 0 && a>0)
+    // {
+    //     y = exp(pow(x,a));
     // }
-    cout << min << endl;
+    // else
+    // {
+    //     y = pow(x,exp(a));
+    // }
+    cout<< "Result: y ="<<y<<endl;
     return 0;
 }
