3a80d49be75ebbb4e097270e64b54a6148cb168a
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-16 12:34:49 +0300

    While ->for

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main() {
     const double E = 0.005;
-    double x = 1;
+
     double s = 0;
-    while (1.0 / x > E) {
-        cout << "current 1/x = " << 1.0/x << endl;
+    double x;
+    for ( x = 1; 1.0 / x > E; x += 1) {
+        cout << "current 1/x = " << 1.0 / x << endl;
         cout << "current suma = " << s << endl;
         s += 1.0 / x;
-        x += 1;
-        cout << "next 1/x = " << 1.0/x << endl;
+        cout << "next 1/x = " << 1.0 / x << endl;
     }
     cout << "suma = " << s << endl;
     cout << "iteration = " << x << endl;
