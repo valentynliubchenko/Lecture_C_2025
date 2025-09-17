e8e180502e55636b477c7f2eab923cea3a384a35
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-17 09:14:24 +0300

    sort v1

--- main.cpp
+++ main.cpp
@@ -3,14 +3,37 @@
 #include <time.h>
 using namespace std;
 
-int main() {
-    unsigned int a = 5;
-    unsigned int b = 7;
-    cout<<a<<" "<<b<<endl;
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
+// 1 2 3 4 0
+// ----------
+// 1 2 3 0 4
+// 1 2 0 3 4
+// 1 0 2 3 4
+// 0 1 2 3 4
 
-    cout<<a<<" "<<b<<endl;
+int main() {
+    const int n = 10;
+    int mas[n];
+    for (int i = 0; i < n; ++i) {
+        mas[i] = rand() % 100;
+    }
+
+    for (int i= 0; i < n; ++i) {
+        cout << mas[i] << " ";
+    }
+    // сортування
+    for (int k = 0; k < n-1; ++k) {
+        for (int i = 0; i < n-1; ++i) {
+            if ( mas[i+1] < mas[i] ) {
+                int c = mas[i+1];
+                mas[i+1] = mas[i];
+                mas[i] = c;
+            }
+        }
+    }
+    cout<<endl;
+    for (int i= 0; i < n; ++i) {
+        cout << mas[i] << " ";
+    }
+
     return 0;
 }
