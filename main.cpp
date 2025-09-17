1047448eaa17d04a13dd8bc4d16f2d88434723d3
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-17 08:43:37 +0300

    min max array

--- main.cpp
+++ main.cpp
@@ -4,21 +4,30 @@
 using namespace std;
 
 int main() {
-    const int n = 5;
-    int mas[n];
+    const int n = 10;
+    long long mas[n];
     for (int i = 0; i < n; ++i) {
-        mas[i] = rand() % 10;
+        mas[i] = rand() % 100;
     }
 
-    for (int i = 0; i < n; ++i) {
+    for (int i= 0; i < n; ++i) {
         cout << mas[i] << " ";
     }
-    cout << endl;
-    int s = 0;
-    for (int i = 0; i < n; ++i) {
-        s += mas[i];
+
+    int min = mas[0];
+    int max = mas[0];
+    for (int i = 1; i < n; ++i) {
+        if (mas[i]<min) {
+            min = mas[i];
+        }
+        if (mas[i]>max) {
+            max = mas[i];
+        }
+
+
     }
-    cout << "suma = " << s << endl;
+    cout << "\nmin = " << min << endl;
+    cout << "\nmax = " << max << endl;
 
     return 0;
 }
