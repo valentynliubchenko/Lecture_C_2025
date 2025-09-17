ff2e55b9cc0f0586b99e939dee30fc9933dd421a
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-17 09:00:14 +0300

    swap ver 3

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-    int a = 5;
-    int b = 10;
+    unsigned int a = 5;
+    unsigned int b = 7;
     cout<<a<<" "<<b<<endl;
-    a = a + b; // a 15 b 10
-    b = a - b; // a 15 b 5
-    a = a - b; // a 10 b 5
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
 
     cout<<a<<" "<<b<<endl;
     return 0;
