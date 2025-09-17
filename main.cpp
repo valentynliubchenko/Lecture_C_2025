a96f72eeb7626f7938eee8d184e53bbc627c6a58
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-17 08:56:09 +0300

    swap

--- main.cpp
+++ main.cpp
@@ -4,34 +4,12 @@
 using namespace std;
 
 int main() {
-    const int n = 10;
-    long long mas[n];
-    for (int i = 0; i < n; ++i) {
-        mas[i] = rand() % 100;
-    }
-
-    for (int i= 0; i < n; ++i) {
-        cout << mas[i] << " ";
-    }
-
-    int imin = 0;
-    int imax = 0;
-    for (int i = 1; i < n; ++i) {
-        if (mas[i]<mas[imin]) {
-            imin = i;
-        }
-        if (mas[i]>mas[imax]) {
-            imax = i;
-        }
-    }
-    cout << "\nmin = " << mas[imin] << endl;
-    cout << "index min = " << imin << endl;
-    cout << "max = " << mas[imax] << endl;
-    cout << "index max = " << imax << endl;
-
-    mas[imax] = 1000;
-    for (int i= 0; i < n; ++i) {
-        cout << mas[i] << " ";
-    }
+    int a = 5;
+    int b = 10;
+    cout<<a<<" "<<b<<endl;
+    int c = a;
+    a=b;
+    b=c;
+    cout<<a<<" "<<b<<endl;
     return 0;
 }
