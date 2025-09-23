3f6614306c7b2ff2306609cfadc14e9f11d853e4
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-23 12:12:27 +0300

    example break exit from loops

--- main.cpp
+++ main.cpp
@@ -9,27 +9,25 @@ using namespace std;
 // 4 return - повертає результ із функції
 
 int main() {
-    bool err1 = false;
-    bool err2 = true;
-    bool err3 = false;
-    cout<<"processing 1"<<endl;
-    if (err1) {
-        goto myend;
+    bool isExit = false;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+                int x = rand() % 100;
+                cout << i << " " << j << " " << k << " " << x << endl;
+                if (x % 13 == 0 && x != 0) {
+                    cout << "Break" << endl;
+                    isExit = true;
+                    break;
+                }
+            }
+            if (isExit) {
+                break;
+            }
+        }
+        if (isExit) {
+            break;
+        }
     }
-    cout<<"processing 2"<<endl;
-    if (err2) {
-        goto myend;
-    }
-    cout<<"processing 3"<<endl;
-
-    if (err3) {
-        goto myend;
-    }
-    cout<<"processing 4"<<endl;
-    goto exit_program;
-    myend:
-    cout<<"Error processing!"<<endl;
-    exit_program:
-    cout<<"Exit!"<<endl;
     return 0;
 }
