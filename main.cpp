2de9fc5bf69b745144514efc01840738afe40b21
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-23 11:44:54 +0300

    example break

--- main.cpp
+++ main.cpp
@@ -3,44 +3,21 @@
 #include <time.h>
 using namespace std;
 
-// 0 4 1 2 3
-// ----------
-// 0 1 2 3 4
-// 0 1 2 3 4
-// 0 1 2 3 4
-// 0 1 2 3 4
+// 1 break     - switch for while do while
+// 2 continue  -  for while do while
+// 3 goto будь де
+// 4 return - повертає результ із функції
 
 int main() {
-    const int n = 10;
-    int mas[n] = {0, 4, 1, 2, 3};
-    for (int i = 0; i < n; ++i) {
-        mas[i] = rand() % 100 -50;
-    }
 
-    for (int i= 0; i < n; ++i) {
-        cout << mas[i] << " ";
-    }
-    // сортування
-    bool isSorted = false;
-    int k = 0;
-    while (!isSorted && k < n-1) {
-        isSorted = true;
-        for (int i = 0; i < n-1-k; ++i) {
-            if ( abs(mas[i+1]) < abs(mas[i]) ) {
-                isSorted =false;
-                int c = mas[i+1];
-                mas[i+1] = mas[i];
-                mas[i] = c;
-            }
+    srand(time(NULL));
+    while (true) {
+        int x = rand()%100;
+        cout << x << " ";
+        if (x%13==0 && x!=0) {
+            break;
         }
-        ++k;
     }
-
-    cout<<endl;
-    for (int i= 0; i < n; ++i) {
-        cout << mas[i] << " ";
-    }
-    cout<<"k = "<<k<<endl;
-
+    cout << "exit"<<endl;
     return 0;
 }
