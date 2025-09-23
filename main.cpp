291fdeaf7646bb0e75d1ca0e505327281dafc31e
valentynliubchenko <lubchenko.val@gmail.com>
2025-09-23 11:36:39 +0300

    sort ver 3

--- main.cpp
+++ main.cpp
@@ -12,28 +12,35 @@ using namespace std;
 
 int main() {
     const int n = 10;
-    int mas[n];
+    int mas[n] = {0, 4, 1, 2, 3};
     for (int i = 0; i < n; ++i) {
-        mas[i] = rand() % 100;
+        mas[i] = rand() % 100 -50;
     }
 
     for (int i= 0; i < n; ++i) {
         cout << mas[i] << " ";
     }
     // сортування
-    for (int k = 0; k < n-1; ++k) {
+    bool isSorted = false;
+    int k = 0;
+    while (!isSorted && k < n-1) {
+        isSorted = true;
         for (int i = 0; i < n-1-k; ++i) {
-            if ( mas[i+1] < mas[i] ) {
+            if ( abs(mas[i+1]) < abs(mas[i]) ) {
+                isSorted =false;
                 int c = mas[i+1];
                 mas[i+1] = mas[i];
                 mas[i] = c;
             }
         }
+        ++k;
     }
+
     cout<<endl;
     for (int i= 0; i < n; ++i) {
         cout << mas[i] << " ";
     }
+    cout<<"k = "<<k<<endl;
 
     return 0;
 }
