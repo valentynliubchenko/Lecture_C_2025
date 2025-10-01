d31c777ac820c076b111b9a699d2674c7a82c2c8
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-01 10:48:38 +0300

    matr sum and neg min max

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ int main() {
     const int n= 3;
     const int m= 4;
     int matr[n][m];
-//    srand(time(NULL));
+    // srand(time(NULL));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            matr[i][j] = rand() % 20 -10;
+            matr[i][j] = rand() % 200 -100;
         }
     }
 
@@ -20,15 +20,26 @@ int main() {
 
     int sum = 0;
     int neg =0;
+    int min = matr[0][0];
+    int max = matr[0][0];
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            sum += matr[i][j];
-            if (matr[i][j] < 0) {
+        for (int ii = 0; ii < m; ii++) {
+            sum += matr[i][ii];
+            if (matr[i][ii] < 0) {
                 neg++;
+            }
+            if (matr[i][ii] > max) {
+                max = matr[i][ii];
+            }
+            if (matr[i][ii] < min) {
+                min = matr[i][ii];
             }
         }
     }
     cout<<endl<<"sum = "<<sum<<endl;
     cout<<endl<<"neg = "<<neg<<endl;
+    cout<<endl<<"min = "<<min<<endl;
+    cout<<endl<<"max = "<<max<<endl;
     return 0;
 }
