30b63969519e147e5a92f7a74ba95a7ab3d4b777
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-01 10:05:12 +0300

    sort array poinnter example 2

--- main.cpp
+++ main.cpp
@@ -5,33 +5,38 @@ using namespace std;
 
 int main() {
     const int n = 10;
-    const int mas[n] = {55,42,11,556,433,1,5,7,3,11};
-    const int *pmas[n];
+    int mas[n] = {55,-42,-11,556,-433,1,5,-7,3,11};
+    int *pmas[n];
     for (int i = 0; i < n; i++) {
-        pmas[i] = mas + i; //&mas[i]
+        cout << mas[i] << "\t";
     }
+    int len =0;
     for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
+        if(mas[i]>0){
+            pmas[len++] = &mas[i];
+        }
     }
     cout << endl;
-    for (int i = 0; i < n; i++) {
-        cout << *pmas[i] << " ";
+    for (int i = 0; i < len; i++) {
+        cout << *pmas[i] << "\t";
     }
-    for (int k = 0; k < n - 1; k++)
-        for (int i = 0; i < n-1-k; i++) {
+    for (int k = 0; k < len - 1; k++)
+        for (int i = 0; i < len-1-k; i++) {
             if (*pmas[i+1] < *pmas[i]) {
-                const int *c = pmas[i+1];
-                pmas[i+1] = pmas[i];
-                pmas[i] = c;
+                int c = *pmas[i+1];
+                *pmas[i+1] = *pmas[i];
+                *pmas[i] = c;
             }
         }
+    //--------------------
+    //
     cout <<"\n--------------"<< endl;
     for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
+        cout << mas[i] << "\t";
     }
     cout << endl;
-    for (int i = 0; i < n; i++) {
-        cout << *pmas[i] << " ";
+    for (int i = 0; i < len; i++) {
+        cout << *pmas[i] << "\t";
     }
     return 0;
 }
