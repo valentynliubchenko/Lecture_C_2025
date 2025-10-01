b0ffbef0991c30c7a3ff2c8b866384fbaeb28168
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-01 10:43:35 +0300

    matr sum and neg

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main() {
-    const int n= 10;
-    const int m= 3;
+    const int n= 3;
+    const int m= 4;
     int matr[n][m];
+//    srand(time(NULL));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            matr[i][j] = rand() % 200 -100;
+            matr[i][j] = rand() % 20 -10;
         }
     }
 
@@ -17,5 +18,17 @@ int main() {
         for (int j = 0; j < m; j++)
             cout << matr[i][j] << "\t";
 
+    int sum = 0;
+    int neg =0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            sum += matr[i][j];
+            if (matr[i][j] < 0) {
+                neg++;
+            }
+        }
+    }
+    cout<<endl<<"sum = "<<sum<<endl;
+    cout<<endl<<"neg = "<<neg<<endl;
     return 0;
 }
