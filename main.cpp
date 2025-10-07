2ff238aa44b1cd9ac10576a8856d4f98e8db51f2
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-07 11:44:53 +0300

    sum  diag2

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-    const int n = 7;
+    const int n = 5;
     int matr[n][n];
     srand(time(NULL));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matr[i][j] = rand() % 200 - 100;
+            matr[i][j] = rand() % 20 - 10;
         }
     }
 
@@ -19,29 +19,14 @@ int main() {
 
     cout<<endl<<endl;
     for (int i = 0; i < n; i++) {
-                cout << matr[i][i] << "\t";
+                cout << matr[i][n-i-1] << "\t";
     }
 
-    int* pmin = &matr[0][0];
-    int* pmax = &matr[0][0];
-    for (int i = 0; i < n; ++i)
-    {
-        if (matr[i][i] < *pmin) pmin = &matr[i][i];// = mas+i;
-        if (matr[i][i] > *pmax) pmax = &matr[i][i];// = mas+i;
+    int s = 0 ;
+    for (int i = 0; i < n; i++) {
+        s += matr[i][n-i-1];
     }
-    cout << "\nmin = " << *pmin
-       << " index min = " << pmin-&matr[0][0] << endl;
-    cout << "\nmax = " << *pmax
-       << " index max = " << pmin-&matr[0][0] << endl;
-
-    int c =*pmin;
-    *pmin =*pmax;
-    *pmax =c;
-    cout<<endl<<endl;
-
-    for (int i = 0; i < n; i++, cout << endl)
-        for (int j = 0; j < n; j++)
-            cout << matr[i][j] << "\t";
+    cout << "\n\ns = "<<s << endl;
 
     return 0;
 }
