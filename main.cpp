60c2a47145a93a153b940ae3b69e53b1208c5080
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-09 14:39:57 +0300

    Приклад вичерпання пам'яті

--- main.cpp
+++ main.cpp
@@ -5,28 +5,17 @@ using namespace std;
 
 int main() {
 
-    int n;
-    cout << "Enter the number of rows: ";
-    cin >> n;
-    //
-   // double* mas = (double*)malloc(sizeof(double) *n); //створення динамічного масиву
-
-    double* mas = new double[n];
+    int n = 100000;
 
 
-    for (int i = 0; i < n; i++) {
-        mas[i] = rand()%10000/100.0;
+    for (int i = 0; i < 10000000; i++) {
+        double* mas = (double*)malloc(n);
+        if (mas == NULL ) {
+            cout<<"Memory allocation failed "<<i<<endl;
+            return 1;
+        }
+
     }
-    for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
-    }
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += mas[i];
-    }
-    cout<<"\nThe sum is: "<<sum;
 
-    //free(mas);
-    delete [] mas;
 return 0;
 }
