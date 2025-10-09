76878d76c54522652f4d8695f69e73ccd0bbe1d0
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-09 14:14:46 +0300

    тип void*

--- main.cpp
+++ main.cpp
@@ -4,25 +4,22 @@
 using namespace std;
 
 int main() {
-    const int n = 3;
-    const int m = 4;
-    const int k = 2;
-    int mas3d[n][m][k];
+    void *p;
+    int *pa;
+    int a = 25;
+    float b =2.5;
+    double c =3.6;
 
-//    srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        for (int ii = 0; ii < m; ii++)
-            for (int iii = 0; iii < k; iii++) {
-            mas3d[i][ii][iii] = rand() % 201 - 100+1;
-        }
-    }
-    cout<<endl<<endl;
-    for (int i = 0; i < n; i++, cout << endl)
-        for (int ii = 0; ii < m; ii++) {
-            for (int iii = 0; iii < k; iii++)
-                cout << mas3d[i][ii][iii] << "\t";
-            cout << endl;
-        }
-    cout<<endl<<endl;
+    pa = &a;
+    p = &a;
+    cout<< *((int*)p)<<endl;
+
+    p = &b;
+    cout<< *((float*)p)<<endl;
+    p = &c;
+    cout<< *((double*)p)<<endl;
+
+    //cout<<*pa<<endl;
+
     return 0;
 }
