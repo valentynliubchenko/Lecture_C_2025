893308851f1658856d47ac57e9ed2bbcba1648cd
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-14 11:44:20 +0300

    динамічний 2d масив. створення та звільнення

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
-#include <math.h>
-#include <time.h>
+#include <exception>
 using namespace std;
 
 int main() {
-
-    int n = 100000;
-
-
-    for (int i = 0; i < 10000000; i++) {
-        double* mas = (double*)malloc(n);
-        if (mas == NULL ) {
-            cout<<"Memory allocation failed "<<i<<endl;
-            return 1;
-        }
-
+    int n;
+    int m;
+    cout << "Enter n>0" << endl;
+    cin >> n;
+    cout << "Enter m>0" << endl;
+    cin >> m;
+    if (n <= 0 || m <= 0) {
+        cout << "Invalid Input" << endl;
+        return 1;
+    }
+    double ** matr = new double*[n];
+    for (int i = 0; i < n; i++) {
+        matr[i] = new double[m];
     }
 
-return 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matr[i][j] = rand() % 10000/100.0;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete[] matr[i];
+    }
+    delete[] matr;
+
+    return 0;
 }
