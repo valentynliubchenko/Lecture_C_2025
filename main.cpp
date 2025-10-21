b9468992b4addf4080ee0dd2e91f63ce5e5aaf41
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-21 12:32:16 +0300

    приклад пошуку символу

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 using namespace std;
 int main()
 {
-    char str[100] = "Help me ";
-    char str2[100]="please";
-    strcat(str, str2 );
+    char str[100] = "Help me, please wweewww!";
+    char * curCh = strchr(str, 'e');
+    int num = 0;
+    while(curCh != NULL) {
+        ++num;
+        *curCh ='*';
+        curCh = strchr(curCh+1, 'e');
+    }
+    cout << num << endl;
     cout << str << endl;
 
     return 0;
