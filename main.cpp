c12461c1e5d7293879e548fab94eee367d06cad0
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-28 12:13:46 +0200

    Сортування рядків

--- main.cpp
+++ main.cpp
@@ -3,33 +3,32 @@
 using namespace std;
 
 int main() {
-    char str[] = "Repetitio este mater studiorumsiue. Help. EIUIUOUOIU eme";
-    char delimiters[] = " ;,!.\t\n"; //розділові знаки
-    char gl[] = "EUIOAeuioa";
-    std::cout << str << std::endl;
-    char *curWord = strtok(str, delimiters); //пошук першого слова
-    char * pMax = NULL;
-    int numMax = 0;
+    const int n = 6;
+    char pibs[n][20] = {
+        "Ivanov K.",
+        "Sidirov Z.",
+        "Abramov R.",
+        "aPetrov Z.",
+        "Mirosh R.",
+        "Rudnev Z."
+    };
+    for (int i = 0; i < n; i++) {
+        cout << pibs[i] << endl;
+    }
 
-    while (curWord) {
-        int num=0;
-        for (int i = 0; curWord[i] != '\0'; i++) {
-            if ( strchr(gl, curWord[i]) != NULL) {
-                ++num;
+    for (int k = 0; k < n - 1; k++) {
+        for (int i = 0; i < n - 1 - k; i++) {
+            if (strcmp(pibs[i + 1], pibs[i]) < 0) {
+                char c[20];
+                strcpy(c, pibs[i + 1]);
+                strcpy(pibs[i+1], pibs[i]);
+                strcpy(pibs[i], c);
             }
         }
-        cout << curWord << "\t" << num << endl;
-        if (pMax ==NULL ||
-            pMax !=NULL &&( num > numMax)) {
-            numMax = num;
-            pMax = curWord;
-        }
-
-        curWord = strtok(NULL, delimiters); //пошук наступного слова
     }
-    if (pMax)
-        cout <<"max = "<< pMax << endl;
-    else
-        cout <<"no found "<< endl;
+    cout<<endl<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << pibs[i] << endl;
+    }
     return 0;
 }
