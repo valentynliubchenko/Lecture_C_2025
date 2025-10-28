c4d520ddd16bf4bdee062b3e23c3ea5b7e8b755d
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-28 11:33:08 +0200

    приклад strtok as trim

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <string.h>
+#include <stdio.h>
 using namespace std;
 int main()
 {
     int col = 0;
-    char str[] = "      Help, me! PleAase! sdf,gsd weArt qw432 xfgbhAdfyj 5654      ";
-    char delimiters[] = " ;!.\t\nA";//розділові знаки
-    std::cout << str << std::endl;
+    char str[] = "      Help,5654      ";
+    char delimiters[] = " ";//розділові знаки
+    printf("[%s]\n",str);
     char* curWord = strtok(str, delimiters);//пошук першого слова
-    while (curWord)
-    {
-        cout << curWord << endl;
-        curWord = strtok(NULL, delimiters);//пошук наступного слова
-    }
+    printf("[%s]",curWord);
 
     return 0;
 }
