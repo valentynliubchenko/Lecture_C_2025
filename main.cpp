f68f0d644a167bf510183ac0cc5afcef81997bba
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-28 11:39:20 +0200

    приклад 4 strtok

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    char str[] = "Repetitio est mater studiorum. eghjttyyt Help. eKJHLKJHLkt esdfgsdfg";
+    char str[] = "RepetitioR este mater studiorum. eghjttyyt HelpH. eKJHLKJHLkt esdfgsdfg";
     char strres[1000]="";
-    char delimiters[] = " ;,!\t\n"; //розділові знаки
+    char delimiters[] = " ;,!.\t\n"; //розділові знаки
     std::cout << str << std::endl;
     char *curWord = strtok(str, delimiters); //пошук першого слова
     while (curWord) {
-        if (curWord[0] == 'e' && curWord[strlen(curWord) -1] =='t') {
+        if (curWord[0] == curWord[strlen(curWord) -1] ) {
 //            cout << curWord << "\t" << strlen(curWord) << endl;
             strcat(strres, curWord);
             strcat(strres, " ");
