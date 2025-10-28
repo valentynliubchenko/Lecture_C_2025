8e177e5935c945cb72678fc25889592019f0f523
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-28 11:38:19 +0200

    приклад 3 strtok

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string.h>
-#include <stdio.h>
 using namespace std;
-int main()
-{
-    int col = 0;
-    char str[] = "      Help,5654      ";
-    char delimiters[] = " ";//розділові знаки
-    printf("[%s]\n",str);
-    char* curWord = strtok(str, delimiters);//пошук першого слова
-    printf("[%s]",curWord);
 
+int main() {
+    char str[] = "Repetitio est mater studiorum. eghjttyyt Help. eKJHLKJHLkt esdfgsdfg";
+    char strres[1000]="";
+    char delimiters[] = " ;,!\t\n"; //розділові знаки
+    std::cout << str << std::endl;
+    char *curWord = strtok(str, delimiters); //пошук першого слова
+    while (curWord) {
+        if (curWord[0] == 'e' && curWord[strlen(curWord) -1] =='t') {
+//            cout << curWord << "\t" << strlen(curWord) << endl;
+            strcat(strres, curWord);
+            strcat(strres, " ");
+        }
+        curWord = strtok(NULL, delimiters); //пошук наступного слова
+    }
+    cout<<strres<<endl;
     return 0;
 }
