61c326406c46a96eaf4848def000204ab16b9ca6
valentynliubchenko <lubchenko.val@gmail.com>
2025-10-28 12:38:44 +0200

    isaplha

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ int main()
     char str[] =   "asdf45645g 3456sD  FDF ";
     int num = 0;
     for (int i = 0; i < str[i]!='\0'; i++) {
-        if (str[i] >= 'A' && str[i] <= 'Z'
-            || str[i] >= 'a' && str[i] <= 'z') {
+        if (isalpha(str[i])) {
             ++num;
         }
     }
