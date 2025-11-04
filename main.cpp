8f011e9cb2496c9c7f9bfdcc98799d53a85f7dd8
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-04 12:43:11 +0200

    Приклад Triangle2

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 #include <string.h>
 using namespace std;
 
-struct Tpoint {
-    int x, y;
-};
-
 struct Triangle {
+    struct Tpoint {
+        int x, y;
+    };
     Tpoint p1; // Вкладена структура
     Tpoint p2; // Вкладена структура
     Tpoint p3; // Вкладена структура
@@ -15,19 +14,24 @@ struct Triangle {
 };
 
 int main() {
-    Triangle t;
+    Triangle t,t2,t3,t4;
     t.p1.x = 1;
     t.p1.y = 2;
     t.p2.x = 3;
     t.p2.y = 4;
     t.p3.x = 5;
     t.p3.y = 6;
-    strcpy(t.comment, "Hello World");
+    strcpy(t.comment, "Red Triangle");
 
     cout << t.p1.x << " " << t.p1.y << endl;
     cout << t.p2.x << " " << t.p2.y << endl;
     cout << t.p3.x << " " << t.p3.y << endl;
     cout << t.comment << endl;
+    Triangle::Tpoint p2;
+    Triangle::Tpoint p3;
+    p2.x = 1;
+    p2.y = 2;
+    char comment[50]; // коментарій
 
     cout << "exit" << endl;
     return 0;
