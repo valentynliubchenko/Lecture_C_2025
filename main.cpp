3158c055b0079e2ab58922f1b20c19c050fa7595
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-04 12:39:56 +0200

    Приклад Triangle

--- main.cpp
+++ main.cpp
@@ -1,45 +1,34 @@
-#include <cmath>
 #include <iostream>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
 
-typedef double (*Tptrfun) (double);
-typedef double (*Tptrfun2) (double, double);
-double myF(double x) {
-    return x*x;
-}
+struct Tpoint {
+    int x, y;
+};
 
-int main()
-{
-    Tptrfun ptrfun;
-    Tptrfun ptrfun2;
+struct Triangle {
+    Tpoint p1; // Вкладена структура
+    Tpoint p2; // Вкладена структура
+    Tpoint p3; // Вкладена структура
+    char comment[50]; // коментарій
+};
 
-    double x =1.2;
-    double y;
-    y =sin(x);
-    cout << y << endl;
-    ptrfun = &sin;
-    y = ptrfun(x);
-    cout << y << endl;
-    y = (*ptrfun)(x);
-    cout << y << endl;
-    y=cos(x);
-    cout << y << endl;
+int main() {
+    Triangle t;
+    t.p1.x = 1;
+    t.p1.y = 2;
+    t.p2.x = 3;
+    t.p2.y = 4;
+    t.p3.x = 5;
+    t.p3.y = 6;
+    strcpy(t.comment, "Hello World");
 
-    ptrfun = &cos;
-    y = ptrfun(x);
-    cout << y << endl;
-    y = (*ptrfun)(x);
-    cout << y << endl;
+    cout << t.p1.x << " " << t.p1.y << endl;
+    cout << t.p2.x << " " << t.p2.y << endl;
+    cout << t.p3.x << " " << t.p3.y << endl;
+    cout << t.comment << endl;
 
-    Tptrfun2 ptrfun3;
-    ptrfun3 = &pow;
-    y = ptrfun3(2, 3.0);
-    cout << y << endl;
-    ptrfun = &myF;
-    y = ptrfun(10);
-    cout << y << endl;
+    cout << "exit" << endl;
     return 0;
-
 }
-
