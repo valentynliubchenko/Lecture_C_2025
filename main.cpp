8bcfdf8738d9740a7272774ca61739e00870d490
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-11 11:45:17 +0200

    Приклад TNod

--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 using namespace std;
-struct T4
+struct Tnod
 {
-    int a;
-    struct T
-    {
-        int b;
-        int k;
-    }obj;
-    char c;
+    int num;
+    char name[100];
+    Tnod* next;
 };
+
 int main()
 {
-    T4 t;
-    t.a = 1;
-    t.obj.b = 2;
-    t.c = 3;
-    cout << sizeof(t) << endl;//12
-    T4::T t2;
-    t2.b = 56;
+
+    Tnod a,b,c;
+    a.num = 1;
+    strcpy(a.name,"name A");
+    b.num = 2;
+    strcpy(b.name,"name B");
+    c.num = 3;
+    strcpy(c.name,"name C");
+
+    cout<<&a<<endl;
+    cout<<&b<<endl;
+    cout<<&c<<endl;
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &a;
+
+    cout<<a.name<< " "<<a.num<<endl;
+    cout<<a.next->name<< " "<<a.next->num<<endl;
+    cout<<b.name<< " "<<b.num<<endl;
+
+    cout<<a.next->next->name<< " "<<a.next->next->num<<endl;
+    cout<<c.name<< " "<<c.num<<endl;
+    cout<<"-------------"<<endl;
+    Tnod * pcur =&a;
+    cout<<pcur->name<< " "<<pcur->num<<endl; //a
+    pcur = pcur->next; //перехід на наступний елемент (b)
+    cout<<pcur->name<< " "<<pcur->num<<endl; //b
+    pcur = pcur->next;  //перехід на наступний елемент (c)
+    cout<<pcur->name<< " "<<pcur->num<<endl; //c
+    pcur = pcur->next;  //перехід на наступний елемент (a)
+    cout<<pcur->name<< " "<<pcur->num<<endl; //a
+
 
     return 0;
 }
