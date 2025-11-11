6692de4cf3c7d003c03ae292587e57f44c2103f8
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-11 12:21:25 +0200

    Приклад Student

--- main.cpp
+++ main.cpp
@@ -1,52 +1,53 @@
+// Створити структуру яка зберігає інформацію про студента. Студент характеризується полями: Ім’я, Прізвище, Середній бал, Адреса проживання.
+// Адреса проживання являється також структурою з полями: Вулиця , Номер будинку, Номер квартири.
+// Створити об’єкт і ввести усю інформацію з клавіатури. Вивести усі поля структури.
+
 #include <iostream>
-#include <time.h>
 #include <string.h>
 using namespace std;
-struct Tnod
-{
-    int num;
-    char name[10];
-    Tnod* next;
+
+struct Adr {
+    char street[20];
+    int numBuild;
+    int numFlat;
 };
 
+struct Student {
+    char name[20];
+    char lastName[20];
+    int avarge;
+    Adr adr;
+};
 int main()
 {
+    //введення
+    char tmpStr[100]="";
+    Student student;
+    cout << "Enter student name: ";
+    cin.getline(student.name,19);
+    cout << "Enter student last name: ";
+    cin.getline(student.lastName,19);
+    cout << "Enter student avarge: ";
+    cin.getline(tmpStr,99);
+    student.avarge = atoi(tmpStr);
+    cout << "Enter Street ";
+    cin.getline(student.adr.street,99);
+    cout << "Enter numBuild ";
+    cin.getline(tmpStr,99);
+    student.adr.numBuild = atoi(tmpStr);
+    cout << "Enter numFlat ";
+    cin.getline(tmpStr,99);
+    student.adr.numFlat = atoi(tmpStr);
 
-    Tnod * pbeg = NULL;
-    int n = 10;
-// створення лінійного списку
-    for (int i = 0; i < n; i++) {
-        Tnod * ptmp = new Tnod;  //1 крок
-        // 2 start
-        ptmp->num = i;
+    //виведення
+    cout<<"Name: "<<student.name<<endl;
+    cout<<"Last Name: "<<student.lastName<<endl;
+    cout<<"Avarge: "<<student.avarge<<endl;
+    cout<<"Street "<<student.adr.street<<endl;
+    cout<<"NumBuild "<<student.adr.numBuild<<endl;
+    cout<<"NumFlat "<<student.adr.numFlat<<endl;
 
-        //name
-        for (int j = 0; j < 9; j++) {
-            ptmp->name[j] = 'A' + rand() % 26;
-        }
-        ptmp->name[9] = '\0';
-        // 2 end
 
-        ptmp->next = pbeg; //3 крок
-        pbeg = ptmp; //4 крок
-
-    }
-
-    // Друкування лінійного списку
-    Tnod * pcur = pbeg;
-    while (pcur != NULL) {
-        cout << pcur->num << " " <<pcur->name <<endl;
-
-        pcur = pcur->next;
-    }
-
-//звільнити лінійний список
-    while (pbeg != NULL) {
-        cout << "Delete "<<pbeg->num << " " <<pbeg->name <<endl;
-        Tnod * ptmp = pbeg; //1
-        pbeg = pbeg->next;//2
-        delete ptmp;//3
-    }
 
 
     return 0;
