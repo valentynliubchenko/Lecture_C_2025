dc70f790ead0f7a227140084cc162019c5bcd862
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-11 11:24:52 +0200

    Приклад вкладеної структури struct T3

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-struct T1
+struct T3
 {
     int a;
     struct
     {
         int b;
-    };
+    }obj, obj2,obj3;
     char c;
 };
 int main()
 {
-    T1 t;
+    T3 t;
     t.a = 1;
-    t.b = 2;
+//    t.b = 2;
     t.c = 3;
-    cout << sizeof(t) << endl;//12
-    system("PAUSE");
+    cout << sizeof(t) << endl;//8
+
     return 0;
 }
