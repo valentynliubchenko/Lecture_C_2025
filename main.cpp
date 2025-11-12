c67ba95f3934e96c7a883a658d86cb35c0bafb36
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-12 08:41:53 +0200

    Приклад факторіал

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ long long  factorial(int n) {
     if (n<=1) return 1;
     return n*factorial(n-1);
 }
+long long  factorialIter(int n) {
+    if (n<=1) return 1;
+    long long res =1;
+    for (int i=2;i<=n;i++) {
+        res *= i;
+    }
+    return res;
+}
 
 int main() {
     for (int x = 1; x <= 20; ++x)
         cout << "x=" << x << "\tfactorial="
-         << factorial(x) << endl;
+         << factorial(x) <<"\t" << factorialIter(x) << endl;
 
 
     return 0;
