05b8f4e238636f1d1c98ee0e4887571cd13f7c99
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-18 11:47:26 +0200

    Приклад функцій з 2Д динамічними масивами

--- main.cpp
+++ main.cpp
@@ -2,28 +2,77 @@
 #include <string.h>
 using namespace std;
 
-void printMas(const int* mas, int size);
-int sum(const int* mas, int size);
-int main() {
-    const int n =10;
-    int mas[n] = {1, 20, 13, 4, 5, 6, 7, 8, 9, 10};
-    printMas(mas, 10);
-    cout << sum(mas, 10) << endl;
-    int mas2[6] = {1, 2, 3, 4, 5, 6};
-    printMas(mas2, 6);
-    cout << sum(mas2, 6) << endl;
+int** createMatr(int n, int m)
+{
+    if (n < 1 || m < 1) return NULL;
+    int** matr = new int* [n];
+    for (int i = 0; i < n; ++i)
+        matr[i] = new int[m];
+    return matr;
+}
 
-    int * mas3 = new int[n];
-    for (int i = 0; i < n; i++) {
-        mas3[i] = rand() % 10;
+void createMatr2(int**&matr, int n, int m)
+{
+    matr = NULL;
+    if (n < 1 || m < 1) return ;
+    matr = new int* [n];
+    for (int i = 0; i < n; ++i)
+        matr[i] = new int[m];
+}
+
+
+
+void deleteMatr(int**& matr, int n)
+/*покажчик matr передається за посиланням, для того щоб можно було його змінити його значення на NULL */
+{
+    for (int i = 0; i < n; ++i)
+        delete[] matr[i];
+    delete[] matr;
+    matr = NULL;
+}
+
+void randMatr(int ** matr, int n, int m, int a, int b)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            matr[i][j] = rand()%(b-a)+a;
+        }
     }
-    printMas(mas3, n);
-    cout << sum(mas3, n) << endl;
-    delete[] mas3;
+}
 
-    ////////////////
-    ///
-    printMas(mas+1, 3);
+void outputMatr( const int * const * matr, int n, int m)
+{
+    for (int i = 0; i < n; ++i, cout << endl) {
+        for (int j = 0; j < m; ++j) {
+            cout << matr[i][j] << "\t";
+        }
+    }
+    cout << endl;
+}
+
+int sumMatr( const int * const * matr, int n, int m)
+{
+    int sum =0;
+    for (int i = 0; i < n; ++i, cout << endl) {
+        for (int j = 0; j < m; ++j) {
+            sum += matr[i][j];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int n, m;
+    cout << "Enter the size of the matrix: ";
+    cin >> n >> m;
+    int ** matr = createMatr(n, m);
+    randMatr(matr, n,m, 5,15);
+    outputMatr(matr, n, m);
+
+    cout << endl << sumMatr(matr, n, m) << endl;
+
+    deleteMatr(matr, n);
+
 
     return 0;
 }
