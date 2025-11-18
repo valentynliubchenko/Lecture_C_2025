ff169e0a0b3c84139f3e74164b59d01d009f3c8f
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-18 11:59:48 +0200

    Покажчик на функцію

--- main.cpp
+++ main.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
-#include <string.h>
+#include <math.h>
 using namespace std;
 
-void randMatr(int * matr, int n, int m, int a, int b)
-{
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            matr[i*m+j] = rand()%(b-a)+a;
-        }
-    }
-}
 
-void outputMatr( const int *matr, int n, int m)
-{
-    for (int i = 0; i < n; ++i, cout << endl) {
-        for (int j = 0; j < m; ++j) {
-            cout << matr[i*m+j] << "\t";
-        }
-    }
-    cout << endl;
-}
 
-int sumMatr( const int * matr, int n, int m)
-{
-    int sum =0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            sum += matr[i*m+j];
-        }
-    }
-    return sum;
-}
+double (*p1) (double);
+double (*p2) (double, double);
+
 
 int main() {
-    const int n =4;
-    const int m =5;
-    int matr[n][m];
-    randMatr(matr[0], n,m, 5,15); // matr[0]   <==>&matr[0][0]
-    outputMatr(matr[0], n, m);
+    p1 = &sin;
+    cout << sin(0.5) << "\n";
+    cout << (*p1)(0.5) << "\n";
+    cout << p1(0.5) << "\n";
 
-    cout << sumMatr(matr[0], n, m) << endl;
+    p1 = &cos;
+    cout << cos(0.5) << "\n";
+    cout << (*p1)(0.5) << "\n";
+    cout << p1(0.5) << "\n";
+
+    p1 = &tan;
+    p2 = &pow;
+    cout << p2(2.0,3) << "\n";
 
     return 0;
 }
