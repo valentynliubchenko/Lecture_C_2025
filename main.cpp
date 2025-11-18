85664043d2de011d3bd5a26b83ad47c087c12ba8
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-18 11:22:51 +0200

    Приклад функцій з масивами2

--- main.cpp
+++ main.cpp
@@ -1,56 +1,44 @@
-#include <cstdint>
 #include <iostream>
-#include <intrin.h>
+#include <string.h>
 using namespace std;
 
-void printMas(const int *mas, int n) {
+void printMas(const int* mas, int size);
+int sum(const int* mas, int size);
+int main() {
+    const int n =10;
+    int mas[n] = {1, 20, 13, 4, 5, 6, 7, 8, 9, 10};
+    printMas(mas, 10);
+    cout << sum(mas, 10) << endl;
+    int mas2[6] = {1, 2, 3, 4, 5, 6};
+    printMas(mas2, 6);
+    cout << sum(mas2, 6) << endl;
+
+    int * mas3 = new int[n];
     for (int i = 0; i < n; i++) {
+        mas3[i] = rand() % 10;
+    }
+    printMas(mas3, n);
+    cout << sum(mas3, n) << endl;
+    delete[] mas3;
+
+    ////////////////
+    ///
+    printMas(mas+1, 3);
+
+    return 0;
+}
+
+void printMas(const int* mas, int size) {
+    for (int i = 0; i < size; i++) {
         cout << mas[i] << " ";
     }
     cout << endl;
 }
 
-void randMas(int *mas, int n) {
-    for (int i = 0; i < n; i++) {
-        mas[i] = rand() % 10;
-    }
-}
-
-int countMas(int *mas, int n) {
-    int c = 0;
-    for (int i = 0; i < n; i++) {
-        if (mas[i] % 2 == 0) {
-            ++c;
-        }
-    }
-    return c;
-}
-
-int sumMas(int *mas, int n) {
+int sum(const int* mas, int size) {
     int sum = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < size; i++) {
         sum += mas[i];
     }
     return sum;
-}
-
-
-int main() {
-    int mas1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int mas2[5] = {43545, 2232, 56564, 445456, 343};
-    printMas(mas1, 10);
-
-    cout << countMas(mas1, 10) << endl;
-    cout << endl;
-    printMas(mas2, 5);
-    cout << countMas(mas2, 5) << endl;
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    int *mas3 = new int[n];
-    randMas(mas3, n);
-    printMas(mas3, n);
-    cout << sumMas(mas3, n) << endl;
-    delete[] mas3;
-    return 0;
 }
