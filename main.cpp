148732f96790106d1667c5a7fb592b1ded38218b
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-18 12:36:58 +0200

    Приклад шаблоних функцій

--- main.cpp
+++ main.cpp
@@ -1,58 +1,57 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-void randMas(int* mas, int n)
+template<typename MyT>
+MyT** createMatr(int n, int m)
 {
+    if (n < 1 || m < 1) return NULL;
+    MyT** matr = new MyT* [n];
     for (int i = 0; i < n; ++i)
-        mas[i] = rand() % 21 - 10;
+        matr[i] = new MyT[m];
+    return matr;
 }
-void outputMas(const int* mas, int n)
+
+template<typename MyT>
+void deleteMatr(MyT**& matr, int n)
+/*покажчик matr передається за посиланням, для того щоб можно було його змінити його значення на NULL */
 {
     for (int i = 0; i < n; ++i)
-        cout << mas[i] << " ";
+        delete[] matr[i];
+    delete[] matr;
+    matr = NULL;
+}
+template<typename MyTempl>
+void randMatr(MyTempl** matr, int n, int m)
+{
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            matr[i][j] = (rand() % 201 - 100)/10.0;
+}
+
+template<typename MyT>
+void outputMatr(MyT** matr, int n, int m)
+{
+    for (int i = 0; i < n; ++i, cout << endl)
+        for (int j = 0; j < m; ++j)
+            cout << matr[i][j] << "\t";
     cout << endl;
 }
-bool cmpLess(int a, int b) {
-    return a < b;
-}
 
-bool cmpGreater(int a, int b) {
-    return a > b;
-}
-
-bool cmpLessAbs(int a, int b) {
-    return abs(a) < abs(b);
-}
-
-typedef bool (*Tcmp)(int, int);
-
-void sortMas(int* mas, int n, Tcmp cmp)
-{
-    for (int k = 0; k < n - 1; ++k)
-        for (int i = 0; i < n - 1 - k; ++i)
-        {
-            if (cmp (mas[i + 1], mas[i]))
-            {
-                int c = mas[i];
-                mas[i] = mas[i + 1];
-                mas[i + 1] = c;
-            }
-        }
-}
 
 int main()
 {
-    const int size = 10;
-    int massive[size];
-    randMas(massive, size);
-    outputMas(massive, size);
-    sortMas(massive, size, cmpLess);
-    outputMas(massive, size);
-    sortMas(massive, size, cmpGreater);
-    outputMas(massive, size);
-    sortMas(massive, size, cmpLessAbs);
-    outputMas(massive, size);
+    int n,m;
+    cout << "Enter the size of the matrix: ";
+    cin >> n >> m;
+    int **matr =createMatr<int>(n, m);
+    randMatr(matr, n, m);
+    outputMatr<int>(matr, n, m);
+    deleteMatr(matr, n);
+
+    double **matrDouble = createMatr<double>(n, m);
+    randMatr(matrDouble, n, m);
+    outputMatr(matrDouble, n, m);
+    deleteMatr(matrDouble, n);
 
     return 0;
 }
