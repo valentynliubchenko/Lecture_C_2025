0e731bd8dbdae55de949258e06d424cb76373a4f
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-25 12:21:18 +0200

    Приклад define SWAP

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 using namespace std;
-#include "funct.h"
 
-start
-    char* argv[])
-старт
-    друкуй<<QWE<<endl;
+int qrt(int x) {return x*x;}
+#define SWAP(a, b, mytype) {\
+        mytype c =b;\
+        b=a;\
+        a=c;\
+}
+
+int main()
+{
+    cout<<qrt(5)<<endl;
+  //  cout<<QRT(5)<<endl;
+    double x =2.5, y =9.4;
+    SWAP(x,y, double)
+
+    cout<<x<<endl;
+    cout<<y<<endl;
+    double x2 =21.5, y2 =91.4;
+    SWAP(x2,y2, double)
+
+    cout<<x2<<endl;
+    cout<<y2<<endl;
+
     return 0;
-end
+}
