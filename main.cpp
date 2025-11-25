19d5d9b4d659e363a2576b906eaaed2e85c712be
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-25 12:33:43 +0200

    Приклад умовної компіляції

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-#define version 3
+#define WIN
+//#undef WIN
+
+// #if defined <==> #ifdef
+// #if !defined <==> #ifndef
 int main()
 {
-#if version == 1
-    cout<<"version 1"<<endl;
-#elif version == 2
-    cout<<"version 2"<<endl;
-#elif version == 3
-    cout<<"version 3"<<endl;
+#ifndef UNIX
+    cout<<"Windows"<<endl;
 #else
-    cout<<"version 4"<<endl;
+    cout<<"unix"<<endl;
 #endif
 
     return 0;
