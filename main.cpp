95fd4e045b0ee789db807999379f13d22ba261f3
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-25 12:27:37 +0200

    Приклад умовної компіляції

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
-#include <iostream>
+#include<iostream>
+#include<stdlib.h>
 using namespace std;
-
-int qrt(int x) {return x*x;}
-#define SWAP(a, b, mytype) {\
-        mytype c =b;\
-        b=a;\
-        a=c;\
-}
-
+#define version 3
 int main()
 {
-    cout<<qrt(5)<<endl;
-  //  cout<<QRT(5)<<endl;
-    double x =2.5, y =9.4;
-    SWAP(x,y, double)
-
-    cout<<x<<endl;
-    cout<<y<<endl;
-    double x2 =21.5, y2 =91.4;
-    SWAP(x2,y2, double)
-
-    cout<<x2<<endl;
-    cout<<y2<<endl;
+#if version == 1
+    cout<<"version 1"<<endl;
+#elif version == 2
+    cout<<"version 2"<<endl;
+#elif version == 3
+    cout<<"version 3"<<endl;
+#else
+    cout<<"version 4"<<endl;
+#endif
 
     return 0;
 }
