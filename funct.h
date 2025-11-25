d8c67314ecc7065c9651b706a4e39a18c423a886
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-25 11:52:13 +0200

    Приклад define

--- funct.h
+++ funct.h
@@ -1,3 +1,8 @@
 
 void hi();
 void hi2();
+#define QWE "Hello fgdfgd World!"
+#define start int main(int argc,
+#define старт {
+#define end }
+#define друкуй cout
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <stdarg.h>
 using namespace std;
 #include "funct.h"
-int main(int argc, char* argv[])
-{
-    hi();
-    hi2();
+
+start
+    char* argv[])
+старт
+    друкуй<<QWE<<endl;
     return 0;
-}
+end
