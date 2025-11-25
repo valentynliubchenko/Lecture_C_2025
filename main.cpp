cb4cab5db3c0e4ad1ead87e702c8612d4dd3d5d6
valentynliubchenko <lubchenko.val@gmail.com>
2025-11-25 15:28:23 +0200

    Приклад перевірки повторного включення файлу

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
-#include<iostream>
-#include "funct.h"
-#include "funct.h"
+#include <iostream>
 using namespace std;
-
-
+#define STR(s) #s
+#define createF(name,type) void name##_##type(type& a,type& b)\
+{\
+char strname[] = STR(name ## _ ## type);\
+cout<< strname << " was create";\
+type c=a;\
+a = b;\
+b = c;\
+}
+createF(swap, int)
+createF(swap, double)
 int main()
 {
-    cout<<sum(5,7)<<endl;
-    cout<<sum(5.2,7.9)<<endl;
-
+    int x = 3, y = 5;
+    swap_int(x, y);
+    cout << endl << x << " " << y << endl;
+    double a = 3.5, b = 5.5;
+    swap_double(a, b);
+    cout<<endl << a<< " " << b << endl;
+    system("Pause");
     return 0;
 }
